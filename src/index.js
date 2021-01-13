29eab9652e821b01c726710cf1afcfaebe25abb3
danajackson2 <danajackson2@gmail.com>
2021-01-13 08:40:12 -0800

    task list functional

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  
+  //Definitions
+  let to_do_list = document.getElementById('tasks')
+  let input = document.querySelector('input#new-task-description')
+  let create = document.querySelector('input#new-task-description + input')
+
+  //Event Handlers
+  function add_item(){
+    let item = document.createElement('li')
+    item.textContent = input.value
+    to_do_list.appendChild(item)
+    x = document.createElement('button')
+    x.textContent = "x"
+    x.className = "delete"
+    document.querySelector('ul li:last-child').appendChild(x)
+    add_x_event_listener()
+    input.value = ""
+  }
+  function delete_task(item){
+    item.parentNode.remove()
+  }
+
+  //Event Listeners
+  create.addEventListener('click', function(e){
+    e.preventDefault()
+    add_item()
+  })
+
+  function add_x_event_listener() {
+    let delete_button = document.querySelector('ul li:last-child button')
+    delete_button.addEventListener('click', function(e) {
+        delete_task(e.target)
+    })
+  }
+})
