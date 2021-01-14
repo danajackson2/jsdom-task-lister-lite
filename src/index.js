da2ac9229071a1550bf86807074da4e04c524a11
danajackson2 <danajackson2@gmail.com>
2021-01-14 11:09:09 -0800

    working on edit stretch goal

--- src/index.js
+++ src/index.js
@@ -1,55 +1,81 @@
 document.addEventListener("DOMContentLoaded", () => {
-
   //Definitions
-  let to_do_list = document.getElementById('tasks')
-  let input = document.querySelector('input#new-task-description')
-  let create = document.querySelector('select + input')
+  let create = document.querySelector('form #submit')
   let select_box = document.querySelector('select')
 
   //Change DOM Elements
-  function textColor(list_element, index){
-    if (index === 0) {
+  function textColor(list_element, priority){
+    if (priority === "high") {
         list_element.style.color = "red"
-        list_element.value = "1"
-      } else if (index === 1) {
-        list_element.style.color = "yellow"
-        list_element.value = "2"
+      } else if (priority === "medium") {
+        list_element.style.color = "#948b26"
       } else {
         list_element.style.color = "green"
-        list_element.value = "3"
       }
   }
-
+  
   //Event Handlers
-  function add_item(i){
+  function add_item(priority, task, duration, due){
     let item = document.createElement('li')
-    item.textContent = input.value
-    textColor(item, i)
-    to_do_list.appendChild(item)
+    item.textContent = `${task.value}  -  will take ${duration.value} hr  -  finish by ${due.value}   `
+    textColor(item, priority)
+    document.querySelector(`ul#tasks #${priority}`).appendChild(item)
     x = document.createElement('button')
     x.textContent = "x"
     x.className = "delete"
-    document.querySelector('ul li:last-child').appendChild(x)
-    add_x_event_listener()
-    input.value = ""
-    // Sort li elements by value 
+    x.style = "margin-left:10px"
+    document.querySelector(`ul#tasks #${priority} li:last-child`).appendChild(x)
+    add_x_event_listener(priority)
+    edit = document.createElement('button')
+    edit.textContent = "edit"
+    edit.className = "edit"
+    edit.style = "margin-left:10px"
+    document.querySelector(`ul#tasks #${priority} li:last-child`).appendChild(edit)
+    add_edit_event_listener(priority)
+    task.value = ""
+    duration.value = ""
+    due.value = ""
   }
 
   function delete_task(item){
     item.parentNode.remove()
+    if (document.querySelector('div#edit').style.display === "") {
+      document.querySelector('div#edit').style.display = "none"
+    }
+  }
+  
+  function edit_task(){
+    
   }
 
   //Event Listeners
   create.addEventListener('click', function(e){
     e.preventDefault()
-    i = select_box.selectedIndex
-    add_item(i)
+    let task = document.querySelector('input#new-task-description')
+    let dur = document.querySelector('input#duration')
+    let due = document.querySelector('input#dueDate')
+    p = select_box.value
+    add_item(p, task, dur, due)
   })
 
-  function add_x_event_listener() {
-    let delete_button = document.querySelector('ul li:last-child button')
+  function add_x_event_listener(priority) {
+    let delete_button = document.querySelector(`ul div#${priority} li:last-child button.delete`)
     delete_button.addEventListener('click', function(e) {
         delete_task(e.target)
     })
   }
+
+  function add_edit_event_listener(priority) {
+    let edit_button = document.querySelector(`ul div#${priority} li:last-child button.edit`)
+    edit_button.addEventListener('click', function(){
+      document.querySelector('div#edit').style.display = ""
+    })
+  }
+
+  document.querySelector('#editSubmit').addEventListener('click', function(e){
+    e.preventDefault()
+    edit_task()
+    document.querySelector('div#edit').style.display = "none"
+  })
+
 })
